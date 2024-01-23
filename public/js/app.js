537c185a9792d05aad42e47d2cbab91408c82d9e
Tookssi <pjubullet816@gmail.com>
2024-01-24 04:00:12 +0900

    front done

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+const socket = io();
+
+const startingSection = document.querySelector('.starting-section');
+const homeBtn = document.querySelector('.home-btn');
+const crazyButton = document.querySelector('crezyButton');
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('A user just connected.');
+
+    socket.on('disconnection', () => {
+        console.log('A user has disconnected.');
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+});
